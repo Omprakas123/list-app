5c8ababe1f7c689872fff8df833931cd2bb5f157
Omprakash kumar <100309892+Omprakas123@users.noreply.github.com>
2023-03-17 01:57:21 +0530

    Add files via upload

---
+++ firstapp/src/Components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <nav>
+      <h1>Tech Star.</h1>
+      <main>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/Contact"}>Contact</Link>
+        <Link to={"/#about"}>About</Link>
+        <Link to={"/#brand"}>Brand</Link>
+        <Link to={"/#services"}>Services</Link>
+      </main>
+    </nav>
+  );
+};
+
+export default Header;
